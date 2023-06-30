a2318502745e8c7d50cd430aca086ab2e0c5a233
Mark Mandriota <mandriotamark@gmail.com>
2023-06-30 23:32:14 +0200

    fixing mac_print_state; adding tests

--- debug.h
+++ debug.h
@@ -8,12 +8,12 @@ const char *opcodes_stringify[] = {"SETR_IMM", "SETC_IMM", "SETT",  "TSET",
 
 #define mac_print_state(mac)                                                   \
   printf("============= STATE =============\n");                               \
-  printf("flags: lo=%d; eq=%d; gr=%d;\n", mac->flag_lo, mac->flag_eq,          \
-         mac->flag_gr);                                                        \
+  printf("flags: lo=%d; eq=%d; gr=%d;\n", mac->flags.lo, mac->flags.eq,        \
+         mac->flags.gr);                                                       \
   printf("row=%d\n", mac->row);                                                \
   printf("col=%d\n", mac->col);                                                \
   printf("temp register ptr: %d\n", mac->tp);                                  \
-  printf("current pos: %lld\n", mac->regs[0]);                                 \
+  printf("current pos: %lld\n", mac->regs.cp);                                 \
   printf("=================================\n");
 
 #endif
